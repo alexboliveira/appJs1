d3ff2c757fff3839b4848d8ab00c1d929d5a6694
alex <oalexbruno@gmail.com>
2023-04-16 23:18:00 -0300

    iniciar

---
+++ js/app.js
@@ -0,0 +1,10 @@
+app = document.getElementById("app");
+novoElemento = document.createElement("div");
+novoElemento.setAttribute("class","header");
+novoElemento.innerHTML = "HEADER";
+app.appendChild(novoElemento);
+
+
+
+
+
