194862d5624dcef68c3ac82b07fd5edb99a7a49a
alexoliveira44 <alex.oliveira44@fatec.sp.gov.br>
2023-04-17 21:46:50 -0300

    testeDados

--- js/app.js
+++ js/app.js
@@ -8,3 +8,31 @@ app.appendChild(novoElemento);
 
 
 
+
+//teste
+
+financas = [{"saida":{"casa": "150", "agua": 50, "luz":130}, "entrada":{"salario": 1500, "comissao": 200}}];
+
+console.log(financas[0].entrada.salario);
+
+
+
+
+fin = {
+    "entrada":{
+        "salario": 1500,
+        "vale": 800,
+        "comissao": 200
+    },
+    "saida":{
+        "casa": 400,
+        "luz": 130,
+        "agua": 50
+    }
+
+}
+
+console.log(fin.entrada.salario);
+
+
+// fim teste
