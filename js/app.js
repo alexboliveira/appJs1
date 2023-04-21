9e2365b4d6a4c1702b7bbc53dc5f9995009ee53d
alexoliveira44 <alex.oliveira44@fatec.sp.gov.br>
2023-04-21 11:19:25 -0300

    entendendo o reduce

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ app.appendChild(novoElemento);
 
 
 
-//teste
+//teste pardrao
 
 financas = [{"saida":{"casa": "150", "agua": 50, "luz":130}, "entrada":{"salario": 1500, "comissao": 200}}];
 
@@ -18,7 +18,7 @@ console.log(financas[0].entrada.salario);
 
 
 
-fin = {
+const fina = {
     "entrada":{
         "salario": 1500,
         "vale": 800,
@@ -32,7 +32,43 @@ fin = {
 
 }
 
-console.log(fin.entrada.salario);
+console.log(fina.entrada.salario);
 
 
-// fim teste
+
+
+
+// fim teste padrao
+
+
+//teste do map
+const fin2 = [
+    {descricao :  "agua", valor: "50"},
+    {descricao :  "luz",  valor: "30"}
+];
+
+function somaVal(item){
+    //return [item.descricao,item.valor].join(" ");
+    return [item.valor * 100];
+    
+}
+
+
+
+console.log(fin2.map(somaVal));
+console.log(fin2[0].descricao);
+// fim teste do map
+
+
+
+// teste do reduce
+console.log("reduce");
+const numbers = [175, 50, 25];
+console.log(numbers.reduce(myFunc));
+
+function myFunc(total, num) {
+  return total + num;
+}
+
+
+//fim teste do reduce
